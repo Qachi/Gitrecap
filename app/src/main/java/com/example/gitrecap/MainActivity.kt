f60326690647b2bc223f129e1add2aba9e7e2cb3
Qachi <akachukwuonyedikachi@gmail.com>
2023-11-15 11:38:54 +0100

    stash them

--- app/src/main/java/com/example/gitrecap/MainActivity.kt
+++ app/src/main/java/com/example/gitrecap/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("THIS IS CONTAINED IN MY NEW COMMIT ")
-        today is a good day
+        println("THIS IS CONTAINED IN MY NEW COMMIT")
     }
 }
